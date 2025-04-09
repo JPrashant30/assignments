64141cfd30a9eb63bf5c677d53e8fb41448f556a
Prashant Jadhav <prashantjadhav.pj63@gmail.com>
2025-04-09 21:05:29 +0530

    Assignment1

---
+++ src/com/cts/testhello/program/Assignment1_DataTaypes.java
@@ -0,0 +1,114 @@
+package com.cts.testhello.program;
+
+public class Assignment1_DataTaypes {
+
+	public static void main(String[] args) {
+	//Temperature of city in degrees celsius 
+		float degreecelsius  = 25.5f;
+		System.out.println("Temprature Of city in Degree celsiusa:"+ degreecelsius);
+		System.out.println ();
+		
+		//Whether a customer has placed an order
+		boolean order1 = true;
+		System.out.println("customer has placed an order1:" + order1);
+		System.out.println ();
+		
+		//Whether a customer has placed an order
+		boolean order2 = false;
+		System.out.println("customer has placed an order2:" + order2);
+		System.out.println ();
+		
+		//Person's phone number: "123-456-7890"
+		int mobileno = 123456789;
+		System.out.println("Person's phone number:" + mobileno);
+		System.out.println();
+		
+		//Amount of money in a customer's bank account
+		float amount  = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account:"+ amount );
+		System.out.println();
+		
+		//Person's email address: "john.doe@example.com"
+		String  email = "john.doe@example.com";
+		System.out.println("Person's email address:"+ email);
+		System.out.println();
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		double latitude = 37.7749;
+		double longitude = -122.4194;
+		System.out.println("Coordinates of a location (latitude, longitude):"+ latitude+longitude);
+		System.out.println(); 
+		
+		//Person's marital status: true or false
+		
+		
+		
+		
+		//Person's occupation: "Software Engineer"
+		String occupation = "Software Engineer";
+		System.out.println("Person's occupation:" + occupation);
+		System.out.println();
+		
+		//Person's favourite colour: "Blue"
+		String colour  = "Blue";
+		System.out.println("Person's favourite colour:" + colour);
+		System.out.println();
+		
+		//Current year: 2023
+		int year = 2023;
+		System.out.println("current year:" + year);
+		System.out.println();
+		
+		//Number of followers on a social media platform: 1,000,000
+		int followers = 1000000;
+		System.out.println("current year:" + followers);
+		System.out.println();
+		
+		//Rating of a movie: 7.5
+	    float rating = 7.5f;
+	    System.out.println("Rating of a movie:"+ rating);
+	    System.out.println();
+	    
+	    //Person's blood type: 'A'
+	    char type = 'A';
+	    System.out.println("Person's blood type:"+type);
+	    System.out.println();
+	    
+	    //Title of a book: "To Kill a Mockingbird"
+	    String  book = "To Kill a Mockingbird";
+	    System.out.println("Title of a book:"+book);
+	    System.out.println();
+	    
+	    //Number of employees in a company: 500
+	    int company = 500;
+	    System.out.println("Number of employees in a company:"+ company);
+	    System.out.println();
+	    
+	    //Time of an event: 2:30 PM
+	    String event = "2:30 PM ";
+	    System.out.println("Time of an event:"+ event );
+	    System.out.println();
+	    
+	    //Name of a country: "United States"
+	    String  country = "United States";
+	    System.out.println("Name of a country:"+country);
+	    System.out.println();
+	    
+	    //.Person's eye color: "Brown"
+	    String  eyecolour  = "Brown";
+	    System.out.println("Person's eye color:"+eyecolour);
+	    System.out.println();
+	    
+	    //Person's birthplace: "New York City"
+	    String    birthplace = "New York City";
+	    System.out.println("Person's birthplace:"+ birthplace);
+	    System.out.println();
+	    
+	    //Distance between two cities: 200.5
+	    float cities = 200.5f;
+	    System.out.println("Distance between two cities:"+ cities);
+	    System.out.println();
+	    
+	    }
+
+}
--- src/com/cts/testhello/program/HelloWorld.java
+++ src/com/cts/testhello/program/HelloWorld.java
@@ -4,32 +4,36 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,world!");
-		
+
 		// print the content and then go to the next line
 		System.out.println("hello,Prashant!");
-		
-		// print the content in same line 
-		System.out.print ("Prashant");
+
+		// print the content in same line
+		System.out.print("Prashant");
 		System.out.print(" Anand Jadhav");
-		System.out.print (" From ,Belgaum");
+		System.out.print(" From ,Belgaum");
 		System.out.println();
-		
-		//Print formated content and then stay on same line 
-		System.out.printf("%s is completed BE, USNO  %d and graduation  academy status is %b","Prashant",25,false);
+
+		// Print formated content and then stay on same line
+		System.out.printf("%s is completed BE, USNO  %d and graduation  academy status is %b", "Prashant", 25, false);
 		System.out.println();
-		
-		//Formated the digitd and then print the formated content and then stay on same line 
-		System.out.format("total bill is%.2f",500.988888);
+
+		// Formated the digitd and then print the formated content and then stay on same
+		// line
+		System.out.format("total bill is%.2f", 500.988888);
 		System.out.println();
-		
-		//join multiple print statements together
+
+		// join multiple print statements together
 		System.out.append("my name is Prashant Jadhav").append(" ,im from belgaum");
 		System.out.println();
-		
+
 		// Print the char based on ASCII value
 		System.out.write(65);
 		System.out.println();
-		
+
+		// adding break point when we are getting an error
+		System.out.println("step 1" + 5 /(4 + 7 + 3));
+
 	}
 
 }
